a642719bc9e58f0d07f5e33987cb02e384aec039
mihirkamdarcouchbase <mihir.kamdar@couchbase.com>
2021-04-30 12:15:19 -0700

    Update systestmon.py
    Added more keywords as requested in CBQE-6860

--- systestmon.py
+++ systestmon.py
@@ -37,7 +37,7 @@ class SysTestMon():
             "component": "memcached",
             "logfiles": "memcached.log.*",
             "services": "all",
-            "keywords": ["CRITICAL", "Basic\s[a-zA-Z]\{10,\}", "Menelaus-Auth-User:\["],
+            "keywords": ["CRITICAL", "Basic\s[a-zA-Z]\{10,\}", "Menelaus-Auth-User:\[", "ERROR", "XERROR", "exception occurred in runloop"],
             "ignore_keywords": None,
             "check_stats_api": False,
             "collect_dumps": False
@@ -112,7 +112,7 @@ class SysTestMon():
             "logfiles": "error.log*",
             "services": "all",
             "keywords": ["rebalance exited", "failover exited with reason", "Basic\s[a-zA-Z]\{10,\}",
-                         "Menelaus-Auth-User:\["],
+                         "Menelaus-Auth-User:\[","Join completion call failed"],
             "ignore_keywords": None,
             "check_stats_api": False,
             "collect_dumps": False
